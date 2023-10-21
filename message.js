183f51ed63a5992610f584b899cdcb5ec21d5031
ahmed outmrhoust <outmaghoustrr@gmail.com>
2023-10-21 23:30:47 +0100

    up 9

---
+++ message.js
@@ -0,0 +1,11 @@
+let yourName = prompt("quel est ton nom ?");
+if(yourName==="Zaineb" || yourName==="zaineb"){
+  alert("Bonjour Madame Zaineb Elmontassir")
+  alert("Si vous Renconter un probléme n'hésitez pas a en faire pare au Devloppeures du site")
+} else if(yourName==="ahmed outmaghoust" ||yourName==="abd elmoughit elhatab"){
+    alert("bien venu dans votre site chér "+yourName)
+    alert("Merci de m' avoir créer Monsieur "+yourName)
+}
+else{
+    alert("Bien venu "+yourName+" sur le site de la Prof Zaineb Elmountassir ");
+}
--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let yourName = prompt("quel est ton nom ?");
-if(yourName==="Zaineb" || yourName==="zaineb"){
-  alert("Bonjour Madame Zaineb Elmontassir")
-  alert("Si vous Renconter un probléme n'hésitez pas a en faire pare au Devloppeures du site")
-} else if(yourName==="ahmed outmaghoust" ||yourName==="abd elmoughit elhatab"){
-    alert("bien venu dans votre site chér "+yourName)
-    alert("Merci de m' avoir créer Monsieur "+yourName)
-}
-else{
-    alert("Bien venu "+yourName+" sur le site de la Prof Zaineb Elmountassir ");
-}
 const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
