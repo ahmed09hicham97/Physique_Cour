7bc2d9773e036cbbd289227a1c635e875f8df090
ahmed outmrhoust <outmaghoustrr@gmail.com>
2023-10-26 21:21:52 +0100

    some changes

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+$("title").on("click", playSounds)
 
-for (let i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", audio);
-}
-
-function audio() {
-  const buttonClicked = this.innerHTML;
-  makeSounds(buttonClicked);
-  buttonAnimation(buttonClicked);
-}
-
-document.addEventListener("keydown", function (event) {
-  makeSounds(event.key);
-  buttonAnimation(event.key);
-});
-
-function makeSounds(key) {
-  switch (key) {
-    case "w":
-      let tom1 = new Audio("sounds/sabilDouaa.mp3");
-      tom1.play();
-      break;
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
+function playSounds() {
+    let tom2 = new Audio("sounds/tahiroL9albi.mp3");
       tom2.play();
-      break;
-    case "s":
-      let tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    default:
-      console.log(key);
-  }
-}
+} 
 
-function buttonAnimation(currentKey) {
-  const activeKey = document.querySelector("." + currentKey);
-  activeKey.classList.add("pressed");
-  setTimeout(function () {
-    activeKey.classList.remove("pressed");
-  }, 150);
-}
